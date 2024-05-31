c8115345f85d857b1acb2159dcf7a8114664d18b
Darshan <87766395+HERO570@users.noreply.github.com>
2024-05-31 18:24:53 +0530

    hi

---
+++ src/server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+
+const app = express();
+
+// Serve static files
+app.use(express.static(path.join(__dirname, '../game')));
+app.use(express.static(path.join(__dirname, '../images')));
+app.use(express.static(path.join(__dirname, '../menu')));
+
+// Define the route for the root URL
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../game/game.html'));
+});
+
+// Endpoint to get words from the text file
+app.get('/words', (req, res) => {
+    fs.readFile(path.join(__dirname, 'data.txt'), 'utf8', (err, data) => {
+        if (err) {
+            return res.status(500).send('Error reading file');
+        }
+        res.json({ words: data.split('\n') });
+    });
+});
+
+// Start the server on port 8080
+app.listen(8080, () => {
+    console.log('Server is listening on port 8080');
+});
